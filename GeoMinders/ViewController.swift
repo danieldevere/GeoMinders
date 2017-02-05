642a3849914e5f45115adefa280d19bc5a6b2fa1
Dan DeVere <danieldevere285@gmail.com>
2017-02-05 15:31:41 -0500

    Initial Commit

---
+++ GeoMinders/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoMinders
+//
+//  Created by DANIEL DE VERE on 2/5/17.
+//  Copyright (c) 2017 DANIEL DE VERE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
